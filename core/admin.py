4ccd814c82a5ab6abaa9e9d73ddaef74118fb316
liesemarques <liee.m.teixeira@gmail.com>
2021-11-29 13:27:48 -0300

    Final

--- core/admin.py
+++ core/admin.py
@@ -4,6 +4,7 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    # Mostra os campos que serao paresentados no painel adminstrativo
     list_display = ('title', 'slug', 'author', 'publish', 'status')
     list_filter = ('status', 'created', 'publish', 'author')
     search_fields = ('title', 'body')
--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,8 @@ from core import views
 app_name = 'core'
 
 urlpatterns = [
-    path('', views.post_list, name='post'),
+    # path('', views.post_list, name='post'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>',
          views.post_detail, name='post_detail'),
 ]
